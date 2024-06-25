4df788e4f648038b767639fdb0fe0a24da824726
Max <eth@0xma.xyz>
2024-06-25 08:44:47 +0300

    add signature validation for webhook endpoint

--- app/api/conf/dynamics/route.ts
+++ app/api/conf/dynamics/route.ts
@@ -1,10 +1,24 @@
 import { HEADERS } from "@/lib/consts";
 import logger from "@/lib/logger";
 import { NextRequest } from "next/server";
+import { Webhooks } from "@octokit/webhooks";
+
+const webhooks = new Webhooks({
+  secret: process.env.DYNAMICS_SECRET as string,
+});
 
 export const POST = async function (request: NextRequest) {
+  // Validate signature
+  const signature = request.headers.get("x-dynamic-signature-256") as string;
   const body = await request.json();
-
+  if (!(await webhooks.verify(body, signature))) {
+    return new Response(JSON.stringify("Unauthorized"), {
+      status: 401,
+      headers: HEADERS,
+    });
+  }
+  //
+  logger.info(request.headers);
   logger.info(body);
   logger.info(body.verifiedCredentials);
   return new Response(JSON.stringify("Done"), {
