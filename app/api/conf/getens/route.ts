8611aaf738422829e07324e8a1c9d65d3edece18
Max <eth@0xma.xyz>
2024-06-26 15:10:43 +0300

    add log to ens route handler

--- app/api/conf/getens/route.ts
+++ app/api/conf/getens/route.ts
@@ -2,8 +2,7 @@ import dotenv from "dotenv";
 import { NextRequest } from "next/server";
 import { HEADERS } from "@/lib/consts";
 import { getENS } from "@/lib/functions";
-
-dotenv.config();
+import logger from "@/lib/logger";
 
 export const GET = async function (request: NextRequest) {
   try {
@@ -15,14 +14,20 @@ export const GET = async function (request: NextRequest) {
         status: 200,
         headers: HEADERS,
       });
+    } else {
+      logger.info("Wallet parameter missing");
+      return new Response(
+        JSON.stringify({ error: "Wallet parameter missing" }),
+        {
+          status: 400,
+          headers: HEADERS,
+        }
+      );
     }
-    return new Response(JSON.stringify([]), {
-      status: 200,
-      headers: HEADERS,
-    });
   } catch (error) {
-    return new Response(JSON.stringify([]), {
-      status: 200,
+    logger.error(`Error in ENS GET handler: ${error}`);
+    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+      status: 500,
       headers: HEADERS,
     });
   }
